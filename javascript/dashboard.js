13ac2dbbd4c5a2b36e1f1d31c983173fe0175077
iKnowJavaScript <remymartins0398@yahoo.com>
2019-02-27 11:30:17 +0100

    added admin dashboard files

---
+++ javascript/dashboard.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#myTab a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    const monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+    const dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    const newDate = new Date();
+    newDate.setDate(newDate.getDate() + 1);    
+    $('#date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+})
