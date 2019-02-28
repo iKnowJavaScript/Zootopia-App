32296c61bc7cf07b63703c1217f5780274a4a733
iKnowJavaScript <remymartins0398@yahoo.com>
2019-02-28 04:40:43 +0100

    successfully sign-in admin

---
+++ javascript/log-in.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+        let $inputPassword = $('#inputPassword');
+        let $inputEmail = $('#inputEmail');
+
+        $inputPassword = $inputPassword.val();
+        $inputEmail = $inputEmail.val();
+
+        localStorage.setItem('username', $inputEmail);
+        localStorage.setItem('password', $inputPassword);
+
+        let password = localStorage.getItem('password');
+        let username = localStorage.getItem('username')
+
+
+        $.ajax({
+          type: 'GET',
+          url: 'http://localhost:3000/admin',
+          success: function (admin) { //animals == booklists && book == animal
+            if(admin.password == password && admin.username == username) {
+                console.log('Good to go')
+                window.location.replace('http://127.0.0.1:5500/Zootopia-App/html/dashboard.html#addNewAnimals')
+            }else{
+                console.log('check matching')
+            }
+          },
+          error: function () {
+            alert('error loading orders');
+          }
+        });
+      });
+    
+})
